f63d7faeffa8d05a0b88464d8695519d4696e983
kratos132 <32889207+kratos132@users.noreply.github.com>
2021-08-29 21:03:28 -0300

    sem token

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client  = new Discord.Client();
 
 const prefixo = '+';
-const token = "NzMwNDYxMzY0ODAzNjAwNTAx.XwX1KA.9eXCvvnIUqMzI5XZaCMWq1frxVQ";
+const token = "";
 ownerID = '729366493682466947'
 
 client.on('message', msg => {
